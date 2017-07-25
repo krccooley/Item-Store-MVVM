48bc8bfab43f54e5d29d1e78eaa0890c407d5628
Kyle Cooley <Kyle Cooley>
2017-07-25 18:46:26 -0400

    Add project files.

---
+++ Item Store MVVM/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace Item_Store_MVVM.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
